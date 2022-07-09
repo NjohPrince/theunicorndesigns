0c2e1193edeb96eb65ec9f8fd8429ab2fa21ca40
NjohPrince <jufredprince@gmail.com>
2022-07-09 19:48:54 +0100

    carousel component

--- components/carousel/Carousel.component.jsx
+++ components/carousel/Carousel.component.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { motion } from "framer-motion";
+
+// stylesheet
+import styles from "./carousel.module.css";
+
+// dummy_data
+import { Categories } from "../../dummy_data/Categories";
 
 const Carousel = () => {
-  return <div>Carousel</div>;
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    // carousel.current.scrollWidth: available carousel width
+    // carousel.current.offsetWidth: what is visible
+
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    return () => {};
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <motion.div ref={carousel} className={styles.carousel__wrapper}>
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className={styles.inner__carousel}
+          whileTap={{ cursor: "grabbing" }}
+        >
+          {Categories &&
+            Categories.length > 0 &&
+            Categories.map((category, index) => {
+              return (
+                <motion.div
+                  key={category.name + "..." + index}
+                  className={styles.carousel__item}
+                >
+                  <button>{category.name}</button>
+                </motion.div>
+              );
+            })}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Carousel;
