1f31ef88f15b55d9eb7ad45b35b91c3b909ea7f8
NjohPrince <jufredprince@gmail.com>
2022-07-13 09:35:56 +0100

    comments

--- filters/Filters.js
+++ filters/Filters.js
@@ -1,3 +1,9 @@
+// filtering from already fetched data
+// not very efficient as the data stored on the CMS may change
+// when there is a  user currently using the platform
+
+// this can be used when the content on the site will not change --static
+
 export const runSearch = (designs, search) => {
   let newlyFilteredArray = [];
 
